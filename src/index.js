9e63c5937deda799bbcc7e589944ccce1e90360f
Fredrik Engstrand <frewacom@gmail.com>
2021-03-17 13:57:58 +0100

    Added function for adding product to database

--- src/index.js
+++ src/index.js
@@ -16,4 +16,34 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 
-console.log(db);
+const collections = {
+    orders: 'Orders',
+    products: 'Products',
+    productSuppliers: 'ProductSuppliers',
+    suppliers: 'Suppliers',
+};
+
+// TODO: Add function for adding new order
+// TODO: Add function for adding new supplier
+// TODO: Add function for adding new product supplier
+
+// TODO: Add functions for updating all of the above
+// TODO: Add functions for removing all of the above
+
+async function addProduct(productId, productCode, quantity, price) {
+    try {
+        // Calling .add() automatically gives the document a unique id.
+        // To give the new document a custom name, you use .doc(<name>).set(<data>)
+        // TODO: Maybe check if the document exists before setting the data?
+        await db.collection(collections.products).doc(productId.toString()).set({
+            productId,
+            productCode,
+            quantity,
+            price,
+        });
+    } catch (error) {
+        console.error('Could not add product:', error);
+    }
+}
+
+// addProduct(0, 1337, 100, 200);
