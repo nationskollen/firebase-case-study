c003741349c38cfe6eb2161949857c183a019c30
Fredrik Engstrand <fredrik@engstrand.nu>
2021-03-17 13:42:24 +0000

    Added add and remove functions for all database types

--- src/index.js
+++ src/index.js
@@ -23,12 +23,43 @@ const collections = {
     suppliers: 'Suppliers',
 };
 
-// TODO: Add function for adding new order
-// TODO: Add function for adding new supplier
-// TODO: Add function for adding new product supplier
-
 // TODO: Add functions for updating all of the above
-// TODO: Add functions for removing all of the above
+
+// Function to generate a random ID for a product/supplier etc
+function genID() {
+    return Math.floor(Math.random() * (1000 - 10) + 10); 
+}
+
+// Function to generate a random ID for a product/supplier etc
+function genProductCode() {
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+    // after the decimal. 
+    return Math.random().toString(36).substr(2, 9);
+}
+
+async function addSupplier(supplierId, name, phone) {
+    try {
+        // Calling .add() automatically gives the document a unique id.
+        // To give the new document a custom name, you use .doc(<name>).set(<data>)
+        // TODO: Maybe check if the document exists before setting the data?
+        await db.collection(collections.suppliers).doc(supplierId.toString()).set({
+            supplierId,
+            name,
+            phone,
+        });
+    } catch (error) {
+        console.error('Could not add supplier:', error);
+    }
+}
+
+async function removeSupplier(supplierId) {
+    try {
+        await db.collection(collections.suppliers).doc(supplierId.toString()).delete();
+    } catch (error) {
+        console.error('Could not remove supplier: ', error);
+    }   
+}
 
 async function addProduct(productId, productCode, quantity, price) {
     try {
@@ -46,4 +77,85 @@ async function addProduct(productId, productCode, quantity, price) {
     }
 }
 
-// addProduct(0, 1337, 100, 200);
+async function removeProduct(productId) {
+    try {
+        // calling .delete() will remove the product from the database db
+        await db.collection(collections.products).doc(productId.toString()).delete();
+    } catch (error) {
+        console.error('Could not remove product:', error);
+    }
+}
+
+// Add new order
+async function addOrder(orderId, productId, quantity) {
+    try {
+        // Calling .add() automatically gives the document a unique id.
+        // To give the new document a custom name, you use .doc(<name>).set(<data>)
+        // TODO: Maybe check if the document exists before setting the data?
+        await db.collection(collections.orders).doc(orderId.toString()).set({
+            orderId,
+            productId,
+            quantity
+        });
+    } catch (error) {
+        console.error('Could not add order:', error);
+    }
+}
+
+async function removeOrder(orderId) {
+    try {
+        await db.collection(collections.orders).doc(orderId.toString()).delete();
+    } catch (error) {
+        console.error('Could not remove order: ', error);
+    }
+}
+
+async function addProductSupplier(productId, supplierId) {
+    try {
+        await db.collection(collections.productSuppliers).doc(productId.toString()).set( {
+            productId,
+            supplierId
+        });
+    } catch(error) {
+        console.error('Could not add Product supplier: ', error);
+    }
+}
+
+async function removeProductSupplier(productId) {
+    try {
+        await db.collection(collections.productSuppliers).doc(productId.toString()).delete();
+    } catch (error) {
+        console.error('Could not remove supplier: ', error);
+    }
+}
+
+async function addTestData() {
+    const productCode = genProductCode();
+    const productId = genID();
+    const supplierId = genID();
+    const orderId = genID();
+    
+    addProduct(productId, productCode, 100, 120);
+    addOrder(orderId, productId, 50);
+    addSupplier(supplierId, "Random supplier", "4204206969");
+    addProductSupplier(productId, supplierId);
+}
+
+// Removes all documents inside a collection
+async function removeDocuments(collectionName, removeFunction) {
+    const collection = await db.collection(collectionName).get();
+    
+    collection.forEach((doc) => {
+        removeFunction(doc.id);
+    });
+}
+
+async function removeTestData() {
+    removeDocuments(collections.orders, removeOrder);
+    removeDocuments(collections.products, removeProduct);
+    removeDocuments(collections.suppliers, removeSupplier);
+    removeDocuments(collections.productSuppliers, removeProductSupplier);
+}
+
+//removeTestData();
+addTestData();
